5460a3a79f70e3261bba6abafbccb1feea11a3d9
ashikkhan <ashikkhan314167@gmail.com>
2025-03-30 13:30:08 +0600

    countup add

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import CountUp from 'react-countup'
 
 export default function Experience() {
     return (
@@ -7,7 +8,7 @@ export default function Experience() {
                 data-aos-duration="3000" className="stats shadow mx-auto flex max-w-2/3 bg-gradient-to-r from-blue-950 via-black to-purple-900">
                 <div className="stat place-items-center">
                     <div className="stat-title">Projects</div>
-                    <div className="stat-value">31K</div>
+                    <div className="stat-value"><CountUp end={55} duration={10}></CountUp></div>
                     <div className="stat-desc">From 2024 to February 2025</div>
                 </div>
 
@@ -19,8 +20,8 @@ export default function Experience() {
 
                 <div className="stat place-items-center">
                     <div className="stat-title">Task done</div>
-                    <div className="stat-value">↘︎ 90 (85%)</div>
-                    <div className="stat-desc">Continue working</div>
+                    <div className="stat-value"><CountUp end={90} duration={10}></CountUp> (<CountUp end={85} duration={15}></CountUp>)%</div>
+                    <div className="stat-desc">Continue working </div>
                 </div>
             </div>
         </div>
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -4,38 +4,37 @@ import { Link } from 'react-router-dom'
 
 export default function Projects() {
     const [projects] = useProjects()
+  return (
+    <div className='bg-gradient-to-r from-blue-950 via-black to-purple-900 min-h-screen'>
+    <h1 className='text-5xl text-center p-8 font-bold  text-cyan-500'>---My Projects---  </h1>
 
-    return (
-        <div className='bg-gradient-to-r from-blue-950 via-black to-purple-900 min-h-screen'>
-            <h1 className='text-5xl text-center p-8 font-bold  text-cyan-500'>---My Projects---  </h1>
-
-            <div data-aos="fade-up"
-                data-aos-duration="3000" className='grid md:grid-cols-2 lg:grid-cols-3 gap-5 mx-5'>
-                {
-                    projects.slice(0, 3).map(project => (
-                        <div key={project.id}>
-                            <div className="card bg-indigo-950 h-[400px]  shadow-sm">
-                                <figure>
-                                    <img
-                                        src={project.image}
-                                        alt="Shoes" />
-                                </figure>
-                                <div className="card-body">
-                                    <h2 className="card-title text-cyan-300">{project.name}</h2>
-                                    <p>{project.features.map(feature => (
-                                        <ul >
-                                            <li>~ {feature}</li>
-                                        </ul>
-                                    ))}</p>
-                                    <div className="card-actions justify-end">
-                                        <Link to={`/projectDetails/${project.id}`}> <button className='btn btn-primary'>Details More</button></Link>
-                                    </div>
-                                </div>
+    <div data-aos="fade-up"
+        data-aos-duration="3000" className='grid md:grid-cols-2 lg:grid-cols-3 gap-5 mx-5'>
+        {
+            projects.slice(0, 3).map(project => (
+                <div key={project.id}>
+                    <div className="card bg-indigo-950 h-[400px]  shadow-sm">
+                        <figure>
+                            <img
+                                src={project.image}
+                                alt="Shoes" />
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title text-cyan-300">{project.name}</h2>
+                            <p>{project.features.map(feature => (
+                                <ul >
+                                    <li>~ {feature}</li>
+                                </ul>
+                            ))}</p>
+                            <div className="card-actions justify-end">
+                                <Link to={`/projectDetails/${project.id}`}> <button className='btn btn-primary'>Details More</button></Link>
                             </div>
                         </div>
-                    ))
-                }
-            </div>
-        </div>
-    )
+                    </div>
+                </div>
+            ))
+        }
+    </div>
+</div>
+  )
 }
