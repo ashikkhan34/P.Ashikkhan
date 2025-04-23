942ef0d8a2d89573adc963869747bee2d227fd91
ashikkhan <ashikkhan314167@gmail.com>
2025-04-23 13:54:27 +0600

    text-update

--- src/MainlayOut/MainlayOut.jsx
+++ src/MainlayOut/MainlayOut.jsx
@@ -5,7 +5,7 @@ import Footer from '../Shared/Footer/Footer'
 
 export default function MainlayOut() {
   return (
-    <div>
+    <div className='text-white'>
         <Navbar></Navbar>
         <Outlet></Outlet>
         <Footer></Footer>
